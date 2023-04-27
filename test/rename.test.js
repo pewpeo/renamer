b8e97e0308788e690644b451d72d42d59b2dfb04
Peter Oltmann <2847698+peteroltmann@users.noreply.github.com>
2023-04-27 08:36:13 +0200

    Add mocha

---
+++ test/rename.test.js
@@ -0,0 +1,9 @@
+import { equal } from 'assert';
+
+describe('Test', function () {
+    describe('#dummy()', function () {
+        it('Dummy test', function () {
+            equal(1, 1);
+        });
+    });
+});
