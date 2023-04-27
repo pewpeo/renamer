66aa533f73f9761c47596eb16ec130f47c89697e
Peter Oltmann <2847698+peteroltmann@users.noreply.github.com>
2023-04-27 08:35:11 +0200

    Refactor to module to make it testable

---
+++ rename.js
@@ -0,0 +1,45 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+
+const umlautList = [
+    { letter: '\u00dc', base: 'Ue' },
+    { letter: '\u00c4', base: 'Ae' },
+    { letter: '\u00d6', base: 'Oe' },
+    { letter: '\u00fc', base: 'ue' },
+    { letter: '\u00e4', base: 'ae' },
+    { letter: '\u00f6', base: 'oe' },
+    { letter: '\u00df', base: 'ss' },
+];
+
+export function replaceUmlauts(str) {
+    umlautList.forEach((umlaut) => {
+        str = str.replaceAll(umlaut.letter, umlaut.base);
+    });
+    return str;
+}
+
+export function renameFileStr(filename, date = new Date()) {
+    filename = replaceUmlauts(filename);
+    // replace 1+ ascii characters and spaces (\s) with '_'
+    // TODO: check which characters should sta
+    // TODO: remove all non-ascii
+    filename = filename.replace(/[\.,"!@#\$%\^&\*\(\)\-=\+;:<>\/\\\|\}\{\[\]`~\s]+/g, '_');
+    filename = date.toISOString().substring(0, 10) + '_' + filename; // prepend date
+
+    return filename;
+}
+
+export function renameFilepathStr(filepath) {
+    // check if files exit
+    const stats = fs.lstatSync(filepath);
+    if (!stats.isFile() && !stats.isDirectory()) {
+        throw new Error(`Error: ${filepath} is not a file or directory.`);
+    }
+
+    const direcory = path.dirname(filepath);
+    const extname = path.extname(filepath);
+    let filename = path.basename(filepath, extname);
+    filename = renameFileStr(filename)
+    
+    return path.join(direcory, filename + extname);
+}
--- renamer.mjs
+++ renamer.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import { exec, execSync, spawn } from 'node:child_process';
-import * as path from 'node:path';
+import { renameFilepathStr } from './rename.js';
 import * as fs from 'node:fs';
 
 // remove node executable and mjs filename
@@ -12,41 +11,10 @@ if (args.length < 1) {
     process.exit(1);
 }
 
-const date = new Date(); // current date
-
-const umlautList = [
-    { letter: '\u00dc', base: 'Ue' },
-    { letter: '\u00c4', base: 'Ae' },
-    { letter: '\u00d6', base: 'Oe' },
-    { letter: '\u00fc', base: 'ue' },
-    { letter: '\u00e4', base: 'ae' },
-    { letter: '\u00f6', base: 'oe' },
-    { letter: '\u00df', base: 'ss' },
-];
-function replaceUmlauts(str) {
-    umlautList.forEach((umlaut) => {
-        str = str.replaceAll(umlaut.letter, umlaut.base);
-    });
-    return str;
-}
-
 try {
     args.forEach((filepath) => {
-        // check if files exit
-        const stats = fs.lstatSync(filepath);
-        if (!stats.isFile() && !stats.isDirectory()) {
-            throw new Error(`Error: ${filepath} is not a file or directory.`);
-        }
+        const newFilepath = renameFilepathStr(filepath);
 
-        const direcory = path.dirname(filepath);
-        const extname = path.extname(filepath);
-        let filename = path.basename(filepath, extname);
-        filename = replaceUmlauts(filename);
-        // replace 1+ ascii characters and spaces (\s) with '_'
-        filename = filename.replace(/[\.,"!@#\$%\^&\*\(\)\-=\+;:<>\/\\\|\}\{\[\]`~\s]+/g, '_');
-        filename = date.toISOString().substring(0, 10) + '_' + filename; // prepend date
-        const newFilepath = path.join(direcory, filename + extname);
-        
         fs.rename(filepath, newFilepath, (err) => {
             console.log(`mv "${filepath}" "${newFilepath}"`);
             if (err) {
