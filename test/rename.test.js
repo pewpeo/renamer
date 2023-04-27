5062beda4ae695f59fd36c88196d7c4342f4b190
Peter Oltmann <2847698+peteroltmann@users.noreply.github.com>
2023-04-27 08:39:11 +0200

    Add test for renaming file string

--- test/rename.test.js
+++ test/rename.test.js
@@ -1,9 +1,18 @@
 import { equal } from 'assert';
+import { renameFileStr } from '../rename.js';
 
-describe('Test', function () {
-    describe('#dummy()', function () {
-        it('Dummy test', function () {
-            equal(1, 1);
+
+const testData = [
+    { filename: 'test 123 ü `',    date: new Date('2023-04-25'), result: '2023-04-25_test_123_ue_' },
+    { filename: 'test    123 ü `', date: new Date('2023-04-25'), result: '2023-04-25_test_123_ue_' },
+];
+
+describe('Rename', function () {
+    describe('#renameFileStr()', function () {
+        it('Replace umlauts, 1+ ascii special characters and spaces (\s) with "_" and prepend the date', function () {
+            testData.forEach((testCase) => {
+                equal(renameFileStr(testCase.filename, testCase.date), testCase.result);
+            });
         });
     });
 });
