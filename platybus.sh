6b8b03eb765b8fba2d255b60bb69dbefe429f314
pewpeo <2847698+pewpeo@users.noreply.github.com>
2026-03-23 19:34:50 +0100

    Add platybus script

---
+++ platybus.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# working directory: script directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "${SCRIPT_DIR}"
+
+# build release binary
+cargo build --release
+
+# create output directory
+mkdir -p dist
+
+# create script file for platybus
+cat >dist/script <<EOF
+#!/usr/bin/env bash
+
+exec ./renamer-rust "\$@"
+EOF
+
+chmod u+x dist/script
+
+# call platybus
+CMD=("platypus -y \
+--droppable \
+--name 'Renamer' \
+--bundle-identifier 'org.pewpeo.renamer' \
+--author 'Peter Oltmann' \
+--app-version $(git describe --tags --always) \
+--interface-type 'Droplet' \
+--interpreter '/bin/sh' \
+--uniform-type-identifiers 'public.item|public.folder' \
+--bundled-file 'target/release/renamer-rust' \
+'dist/script' 'dist/Renamer'")
+
+echo "${CMD}"
+eval ${CMD}
